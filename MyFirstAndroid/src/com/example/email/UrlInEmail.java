51ccea59531d9a3239ab6d0ba576bd5c6128b7fc
hester0207 <huxiaomi0207@gmail.com>
2013-12-16 17:23:34 +0800

    url in email

---
+++ MyFirstAndroid/src/com/example/email/UrlInEmail.java
@@ -0,0 +1,42 @@
+package com.example.email;
+
+import java.io.UnsupportedEncodingException;
+
+import org.kobjects.base64.Base64;
+
+import com.example.entity.PersonModel;
+
+public class UrlInEmail {
+	public static String getPasswordUrl()
+	{
+		String url = "http://hyacinth02070.oicp.net/YourPassword.aspx?id=";
+		byte[] encode;
+		try {
+			encode = PersonModel.weibaoID.getBytes("UTF-8");
+			// base64 加密  
+			String idEncode = new String(Base64.encode(encode));  
+			return url+idEncode;
+		} 
+		catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}  
+	}
+	public static String confrimUrl()
+	{
+		String url = "http://hyacinth02070.oicp.net/Confirm.aspx?id=";
+		byte[] encode;
+		try {
+			encode = PersonModel.weibaoID.getBytes("UTF-8");
+			// base64 加密  
+			String idEncode = new String(Base64.encode(encode));   
+			return url+idEncode;
+		} 
+		catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}  
+	}
+}
